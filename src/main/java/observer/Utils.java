a6d17db47e38566427bfa096042624abba3e4ad8
vaba1010 <valentin.baranov@netcracker.com>
2017-10-30 15:13:52 +0400

    Recursive call changed to iterative one

--- src/main/java/observer/Utils.java
+++ src/main/java/observer/Utils.java
@@ -4,10 +4,14 @@ import java.awt.*;
 
 public class Utils {
     public static Color pickupDifferentRandomColor(Color currentColor) {
-        Color pickedColor = new Color((int)(Math.random() * 0x1000000));
-        if(pickedColor.equals(currentColor)) {
-            pickupDifferentRandomColor(currentColor);
+        Color pickedColor = pickupRandomColor();
+        while (pickedColor == currentColor) {
+            pickedColor = pickupRandomColor();
         }
         return pickedColor;
+    }
+
+    private static Color pickupRandomColor() {
+        return new Color((int)(Math.random() * 0x1000000));
     }
 }
